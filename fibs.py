cf809d9cc09a68add89ac44819a80fc7f9ee8d6b
ThorChadMerlin <merlinlindsay@hotmail.com>
2022-09-20 22:21:38 +0100

    fibs short

--- fibs.py
+++ fibs.py
@@ -1,15 +1,15 @@
 from math import sqrt
 
 def fibs(n):
-	return (((1+sqrt(5))/2)**n - ((1-sqrt(5))/2)**n)/sqrt(5)
-
-phi_pos = 1.618033988749895
-phi_neg = -0.6180339887498949
-root_5 = 2.23606797749979
-
-def fibs_const(n):
-	return (phi_pos**n - phi_neg**n)/root_5
+	if n == 0:
+		return 0
+	a = 1
+	b = 1
+	for _ in range(2, n):
+		c = a + b
+		a = b
+		b = c
+	return b
 
 val = 30
 print(fibs(val))
-print(fibs_const(val))
